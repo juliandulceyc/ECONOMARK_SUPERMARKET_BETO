986c9a28bb20ef85c4d3304d1d907fc2999bf767
minino-314220 <jeffersoncontreras2004@gmail.com>
2024-09-19 21:18:32 -0500

    Update App.js

--- React/App.js
+++ React/App.js
@@ -1,11 +1,14 @@
-import { Routes, Route, BrowserRouter} from 'react-router-dom';
+import { Routes, Route, BrowserRouter, Navigate} from 'react-router-dom';
 import ShowProducts from './components/ShowProducts';
+import Login from './Login';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<ShowProducts></ShowProducts>}></Route>
+        <Route path='/login' element={<Login/>}></Route>
+        <Route path='/' element={<Navigate to="/login" />}></Route>
+        <Route path='/components/ShowProducts' element={<ShowProducts/>}></Route>
       </Routes>
     </BrowserRouter>
   )
